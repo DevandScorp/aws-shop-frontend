a5131a2e62986cbf8244f7fb1eab1365be9bb9df
devandscorp <artemda459@gmail.com>
2020-11-08 21:32:59 +0300

    feat: edit product rendering logic

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -46,7 +46,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={product.imageUrl}
+              image={product.image || 'https://source.unsplash.com/random'}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
