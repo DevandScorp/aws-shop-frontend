c46b4de15c03d83c05977c61a97e342e8e41857e
devandscorp <artemda459@gmail.com>
2020-11-15 16:26:14 +0300

    feat: integrate frontend with createProduct lambda

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://fv8w9bbv2m.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://fv8w9bbv2m.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
